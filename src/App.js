ba1c3e83eaec1253eacd4dc9eb001aa97da0c7b4
Watayo <ryo0616mani@gmail.com>
2020-03-18 13:16:37 +0900

    create class components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import { findAllByLabelText } from '@testing-library/react';
 
 
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={() => { console.log("I am clicked!") }} />
+//     </React.Fragment >
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => { console.log("I am clicked!") }} />
-    </React.Fragment >
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return (
+    <div>Meow!</div>
+  )
+}
 export default App;
