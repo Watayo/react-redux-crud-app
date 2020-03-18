cb4225f9f9330d0e5b3d93f1c556efb90f78b7d3
Watayo <ryo0616mani@gmail.com>
2020-03-18 18:24:33 +0900

    learnstate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 import { findAllByLabelText } from '@testing-library/react';
 
@@ -13,32 +12,31 @@ import { findAllByLabelText } from '@testing-library/react';
 //     </React.Fragment >
 //   );
 // }
+const App = () => (<Counter></Counter>)
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Ryo", age: 20 },
-    { name: "NoName" }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count : {this.state.count} </div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return (
-    <div>Hi! I am {props.name}, and {props.age} yesrs old.</div>
-  )
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
 export default App;
